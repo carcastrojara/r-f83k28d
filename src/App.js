a5adb064fa4a583849a10068445599b625b50989
Carlos <carcastrojara@gmail.com>
2023-04-06 10:28:16 -0400

    Solved

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value + 1
+    }));
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.handleIncrement}>Incrementa</button>
       </div>
     );
   }
